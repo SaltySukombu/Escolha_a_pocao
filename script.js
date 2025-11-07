c5754438da8189f3f41cb320e5791db33f5fa156
Rafael de Lima Rueckl <rafael.rueckl@hotmail.com>
2025-11-07 20:40:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function beberPocao(tipo){
+
+    if (tipo === "vermelha"){
+        alert("Você tomou a poção de vida!");
+    }
+
+    else if(tipo === "azul"){
+        alert("Você recuperou mana!");
+    }
+    else{
+        alert("Oh não! voce tomou veneno você perdeu o jogo!");
+    }
+}
